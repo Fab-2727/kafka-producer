c7661ede31ea01bd362512adc0f48eaeb92523a7
Fab-2727 <fabriziolionelsosa27@gmail.com>
2021-06-05 21:01:53 -0300

    Fix .properties and test

--- src/test/bash/test.sh
+++ src/test/bash/test.sh
@@ -24,11 +24,18 @@ tee_process_pid=$!
 sleep 16
 
 function test-payload () {
-    while [[ "$counter" -lt 2 ]]
+    while [[ "$counter" -lt 1 ]]
     do
         curl -X POST \
         -H 'Content-type: text/plain' \
-        --data "[$counter] Hola mundo, probando" \
+        --data "[$counter] Message send to Kafka Producer using 'Content-type: text/plain'." \
+        'localhost:8080/api/v1/kafka/message'
+        
+        sleep 3
+        
+        curl -X POST \
+        -H "Content-type: text/plain" \
+        --data '{"dni":40111222,"phone_number":"1122334455","name":"John","lastname":"Doe","birthDate":"01/08/2009"}' \
         'localhost:8080/api/v1/kafka/message'
         
         counter=$((counter+1))
